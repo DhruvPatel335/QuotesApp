dadbb5ca1076434fe279d41889d9b93cb2f7914c
Dhruv Patel <dhruvp5200@gmail.com>
2024-10-27 16:30:08 +0530

    Modified theme using jetpack compose

--- app/src/main/java/com/quotes/app/ui/theme/Theme.kt
+++ app/src/main/java/com/quotes/app/ui/theme/Theme.kt
@@ -57,7 +57,7 @@ fun QuotesAppTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = Purple40.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
